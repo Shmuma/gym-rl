d9a24e3388b685892b958d99e7871220046deddc
Max Lapan <max.lapan@gmail.com>
2016-05-29 13:30:00 +0200

    Forward net

--- dqn_train.py
+++ dqn_train.py
@@ -2,10 +2,18 @@ import gym
 import tensorflow as tf
 import argparse
 
+from lib import dqn
+
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
 
-    for episode in range(20):
+    input_size = env.observation_space.shape[0]
+    output_size = env.action_space.n
+    net = dqn.DenseDQN(input_size, output_size, batch_size=16, neurons=(128, 64))
+
+    print net
+
+    for episode in range(1):
         observation = env.reset()
         step = 0
         while True:
--- lib/dqn.py
+++ lib/dqn.py
@@ -13,6 +13,13 @@ class DQN:
         # second net, we use to make a forecasts.
         self.next_input_t = tf.placeholder(tf.float32, shape=(self.batch_size, self.input_len), name="next_input")
 
+    def __str__(self):
+        return """DQN: input={input_len}, output={output_len}, batch={batch_size}
+    input_t={input_t}
+    next_input_t={next_input_t}
+        """.format(input_len=self.input_len, output_len=self.output_len, batch_size=self.batch_size,
+                   input_t=self.input_t, next_input_t=self.next_input_t)
+
 
 class DenseDQN(DQN):
     def __init__(self, input_len, output_len, batch_size, neurons, dropout_keep_prob=0.5):
@@ -23,6 +30,13 @@ class DenseDQN(DQN):
         self.qvals_t = self._make_forward_net(self.input_t, is_trainable=True)
         self.next_qvals_t = self._make_forward_net(self.next_input_t, is_trainable=False)
 
+    def __str__(self):
+        return DQN.__str__(self) + "\n" + \
+        """DenseDQN: neurons={neurons}, dropout={dropout_keep_prob}
+    qvals_t={qvals_t}
+    next_qvals_t={next_qvals_t}
+        """.format(neurons=self.neurons, dropout_keep_prob=self.dropout_keep_prob,
+                   qvals_t=self.qvals_t, next_qvals_t=self.next_qvals_t)
 
     def _make_forward_net(self, input_t, is_trainable):
         w_attrs = {'trainable': is_trainable, 'name': 'w'}
@@ -42,9 +56,10 @@ class DenseDQN(DQN):
             l0_out = tf.nn.relu(v, name="L0")
             if is_trainable:
                 tf.contrib.layers.summarize_activation(l0_out)
-        layer_input = l0_out
+        layer_output = l0_out
 
         for layer_index in range(len(self.neurons)-1):
+            layer_input = layer_output
             with tf.name_scope("L{}{}".format(layer_index, suff)):
                 w = tf.Variable(init((self.neurons[layer_index], self.neurons[layer_index+1])), **w_attrs)
                 b = tf.Variable(tf.zeros((self.neurons[layer_index+1], )), **b_attrs)
@@ -55,3 +70,11 @@ class DenseDQN(DQN):
                     if self.dropout_keep_prob is not None and self.dropout_keep_prob < 1.0:
                         layer_output = tf.nn.dropout(layer_output, self.dropout_keep_prob)
                     tf.contrib.layers.summarize_activation(layer_output)
+
+        with tf.name_scope("LOut" + suff):
+            w = tf.Variable(init((self.neurons[-1], self.output_len)), **w_attrs)
+            b = tf.Variable(tf.zeros((self.output_len, )), **b_attrs)
+            v = tf.matmul(layer_output, w) + b
+            layer_output = tf.identity(v, "out")
+
+        return layer_output
